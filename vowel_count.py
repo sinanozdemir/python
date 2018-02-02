a3274d78ede73373d971304d86170991bcd3c329
sinanozdemir <sinanozdem@gmail.com>
2018-02-02 15:35:02 +0000

    vowel_count.py committed

---
+++ vowel_count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+def vowelCount():
+	vowels = "aeiuo"
+	user_string = input("Enter a sentence or a word: ")
+	results = dict()
+	
+	for chars in set(user_string.lower()):
+		if chars in vowels:
+			results[chars] = list(user_string.lower()).count(chars)
+	print(results)
+
+vowelCount()
